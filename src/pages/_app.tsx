c8cd1cbb01b07b60adbf2e47ea8d32b341d5324a
Thomas Bird <thomas.bird1984@gmail.com>
2022-08-26 22:41:36 -0400

    Adding the initial codebase

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import '../styles/index.scss'
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
+
+export default App
